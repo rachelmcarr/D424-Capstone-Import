7ede4a4603055f9e2b3c3a0e203f0122902065bf
unknown <rcar552@wgu.edu>
2025-06-06 14:24:48 -0500

    Validation added for email, phone, state, zip.

--- src/app/components/customer-form/customer-form.component.ts
+++ src/app/components/customer-form/customer-form.component.ts
@@ -30,6 +30,14 @@ export class CustomerFormComponent implements OnChanges {
     updatedAt: ''
   };
 
+  states: string[] = [
+    'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+    'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+    'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+  ];
+
   constructor(private customerService: CustomerService,
     private datePipe: DatePipe
   ) {}
